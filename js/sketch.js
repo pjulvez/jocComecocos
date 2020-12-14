860a9c3a6eebb5f1cb7af659c7d6d7b6975036f1
pjulvez <pjulvez@almata.cat>
2020-12-14 19:38:03 +0100

    bucle de les roques

--- js/sketch.js
+++ js/sketch.js
@@ -2,7 +2,14 @@ function setup() {
   createCanvas(400, 400);
 }
 
+
 function draw() {
   background(220);
-}
+  //  roca.show();
+  //with i i count the rows, with j the columns
+  for (var i = 0; i < arrayRocasMapa.length; i++) {
+    console.log("Imprimo una roca:" + i);
+    arrayRocasMapa[i].show();
+  }
+
 array Maze = new Array[0,0,0,1,1,1,0,0,0,0,0,0,1,1,1,1];
