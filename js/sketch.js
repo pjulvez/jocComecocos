de52df899f16111bc8d8cb7693caa6552bc9fe88
pjulvez <pjulvez@almata.cat>
2021-01-11 18:32:50 +0100

    configuracio menjar.js

--- js/sketch.js
+++ js/sketch.js
@@ -1,31 +1,43 @@
 
-
 var arrayRocasMapa = [];
-function preload() {
-  rocaImage = loadImage("/images/roca.bmp");
+var arrayMenjarMapa = [];
+var myMaze = new Maze();
+var rocaImage;
+var menjarImage;
 
+function preload() {
+  rocaImage = loadImage("images/roca.bmp");
+  menjarImage = loadImage("images/food.png");
 //  font = loadFont('assets/SourceSansPro-Regular.otf');
 }
 function setup() {
-  createCanvas(COLUMNS*IMAGE_SIZE, ROWS*IMAGE_SIZE);
+  createCanvas(myMaze.rows*myMaze.imageSize, myMaze.columns*myMaze.imageSize);
 
-//  myMaze = new Maze();
+  for (let i = 0; i < myMaze.maze.length; i++){
 
-  for (var i = 0; i < myMaze.rows; i++){
-    for (var j = 0; j < myMaze.columns; j++) {
+    for (let j = 0; j < myMaze.maze.length; j++) {
+          if(myMaze.maze[i][j]=== 1){
+            let rocatemp = new Roca()
+            arrayRocasMapa.push( new Roca(i*myMaze.imageSize, j*myMaze.imageSize))
+
+          }
 
     }
   }
+
 }
 function draw() {
-  clear();
-  console.log("Entra a  draw");
+
+
   background(0);
-  //  roca.show();
+  //roca.show();
+
   //with i i count the rows, with j the columns
-  for (var i = 0; i < arrayRocasMapa.length; i++) {
+
+  for (let i = 0; i < arrayRocasMapa.length; i++) {
     console.log("Imprimo una roca:" + i);
     arrayRocasMapa[i].show();
   }
+
 }
 //array Maze = new Array[0,0,0,1,1,1,0,0,0,0,0,0,1,1,1,1];
